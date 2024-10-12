18ad38dae4f1e63da8ad39b64b32f0c970e4a3ac
Prasad Wable <prasadwable31@gmail.com>
2024-10-12 16:11:52 +0530

    Add files via upload

---
+++ Combination3.java
@@ -0,0 +1,19 @@
+
+// hierachal
+
+ // Case 3
+  
+class Demo
+{
+
+}
+
+class Hello extends Demo
+{
+
+}
+
+class Marvellous extends Demo
+{
+
+}
---
+++ Combination4.java
@@ -0,0 +1,18 @@
+
+ // Case 4
+//  Multiple    not allowed
+  
+class Demo
+{
+
+}
+
+class Hello 
+{
+
+}
+
+class Marvellous extends Demo, Hello        // Error
+{
+
+}
