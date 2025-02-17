46cf076019e8e2759cd2ba8a3635799bd4f70e2f
Prasad Wable <prasadwable31@gmail.com>
2025-02-17 21:40:00 +0530

    Add files via upload

---
+++ Mathematical_fun.java
@@ -0,0 +1,22 @@
+
+class Mathematical_fun
+{
+    public static void main(String ar[])
+    {
+
+        double a = 10;
+        double b = 20;
+        double c = 10.5;
+
+        System.out.println("Minimun : "+Math.min(a,b));
+        System.out.println("Maximun : "+Math.max(a,b));
+        System.out.println("Power : "+Math.pow(a,b));
+        System.out.println("Square root : "+Math.sqrt(a));
+        System.out.println("Round : "+Math.round(c));
+        System.out.println("Abstract : "+Math.abs(a));
+        System.out.println("Logarithm : " + Math.log(a));
+        System.out.println("Logarithm 10 : " + Math.log10(a));
+        System.out.println("exp : " +Math.exp(a));
+
+    }
+}
